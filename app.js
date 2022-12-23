c471b7418546a3867b93805bcbe63095da9dcadf
Carter Geile <cartergeile@l.lan>
2022-12-22 23:00:44 -0600

    second attemp at creating game, have to be close, stumped

--- app.js
+++ app.js
@@ -1,340 +1,91 @@
-flag = 1
+const cells = document.querySelectorAll('.cell')
+const statusText = document.querySelector('#statusText')
+const restartBtn = document.querySelector('#restartBtn')
 
-function myFunc () {
-  
-  
-    const b1 = document.getElementById("b1").value
-    const b2 = document.getElementById("b2").value
-    const b3 = document.getElementById("b3").value
-    const b4 = document.getElementById("b4").value
-    const b5 = document.getElementById("b5").value
-    const b6 = document.getElementById("b6").value
-    const b7 = document.getElementById("b7").value
-    const b8 = document.getElementById("b8").value
-    const b9 = document.getElementById("b9").value
-}
-// player x wins
-if ((b1 == 'x' || b1 == 'X') && (b2 == 'x' || b2 == 'X') && (b3 == 'x' || b3 == 'X')) {
-  document.getElementById('print')
-      .innerHTML = "Player X won";
-    document.getElementById("b4").disabled = true;
-    document.getElementById("b5").disabled = true;
-    document.getElementById("b6").disabled = true;
-    document.getElementById("b7").disabled = true;
-    document.getElementById("b8").disabled = true;
-    document.getElementById("b9").disabled = true;
-    window.alert('player X won!');
-}
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
-else if ((b1 === 'x' || b1 === 'X') && (b4 === 'x' || b4 === 'X') && (b7 === 'x' || b7 === 'X')) {
-  document.getElementById('print').innerHTML = "Player X won";
-    document.getElementById('b2').disabled = true
-    document.getElementById('b3').disabled = true
-    document.getElementById('b5').disabled = true
-    document.getElementById('b6').disabled = true
-    document.getElementById('b8').disabled = true
-    document.getElementById('b9').disabled = true
-    window.alert('player X won!')
+let options = ['', '', '', '', '', '', '', '', '']
+let currentPlayer = 'X'
+let running = false
+
+initializeGame()
+
+function initializeGame() {
+  cells.forEach(cell => cell.addEventListener('click', cellClicked))
+  restartBtn.addEventListener('click', restartGame)
+  statusText.textContent = `${currentPlayer}'s turn`
+  running = true
 }
 
-else if ((b4 === 'x' || b4 === 'X') && (b5 === 'x' || b5 === 'X') && (b6 === 'x' || b6 === 'X')) {
-  document.getElementById('print').innerHTML = "Player X won";
-    document.getElementById('b1').disabled = true
-    document.getElementById('b2').disabled = true
-    document.getElementById('b3').disabled = true
-    document.getElementById('b7').disabled = true
-    document.getElementById('b8').disabled = true
-    document.getElementById('b9').disabled = true
-    window.alert('player X won!')
+function cellClicked() {
+  const cellIndex = this.getAttribute('cellIndex')
+
+  if (options[cellIndex] != '' || !running) {
+    return
+  }
+
+  updateCell(this, cellIndex)
+  checkWinner()
+  changePlayer()
 }
 
-else if ((b7 === 'x' || b7 === 'X') && (b8 === 'x' || b8 === 'X') && (b9 === 'x' || 93 === 'X')) {
-  document.getElementById('print').innerHTML = "Player X won";
-    document.getElementById('b1').disabled = true
-    document.getElementById('b2').disabled = true
-    document.getElementById('b3').disabled = true
-    document.getElementById('b4').disabled = true
-    document.getElementById('b5').disabled = true
-    document.getElementById('b6').disabled = true
-    window.alert('player X won!')
+function updateCell(cell, index) {
+  options[index] = currentPlayer
+  cell.textContent = currentPlayer
+
 }
 
-else if ((b2 === 'x' || b2 === 'X') && (b5 === 'x' || b5 === 'X') && (b8 === 'x' || b8 === 'X')) {
-  document.getElementById('print').innerHTML = "Player X won";
-    document.getElementById('b1').disabled = true
-    document.getElementById('b3').disabled = true
-    document.getElementById('b4').disabled = true
-    document.getElementById('b6').disabled = true
-    document.getElementById('b7').disabled = true
-    document.getElementById('b9').disabled = true
-    window.alert('player X won!')
+function changePlayer() {
+  currentPlayer = (currentPlayer == 'X') ? 'O' : 'X'
+  statusText.textContent = `${currentPlayer}'s turn`
 }
 
-else if ((b3 === 'x' || b3 === 'X') && (b6 === 'x' || b6 === 'X') && (b9 === 'x' || b9 === 'X')) {
-  document.getElementById('print').innerHTML = "Player X won";
-    document.getElementById('b1').disabled = true
-    document.getElementById('b2').disabled = true
-    document.getElementById('b4').disabled = true
-    document.getElementById('b5').disabled = true
-    document.getElementById('b7').disabled = true
-    document.getElementById('b8').disabled = true
-    window.alert('player X won!')
-}
+function checkWinner() {
+  let roundWon = false
 
-else if ((b1 === 'x' || b1 === 'X') && (b5 === 'x' || b5 === 'X') && (b9 === 'x' || b9 === 'X')) {
-  document.getElementById('print').innerHTML = "Player X won";
-    document.getElementById('b2').disabled = true
-    document.getElementById('b3').disabled = true
-    document.getElementById('b4').disabled = true
-    document.getElementById('b6').disabled = true
-    document.getElementById('b7').disabled = true
-    document.getElementById('b8').disabled = true
-    window.alert('player X won!')
-}
+  for (let i = 0; i < winConditions.length; i++) {
+    const condition = winConditions[i]
+    const cellA = options[condition[0]]
+    const cellB = options[condition[1]]
+    const cellC = options[condition[2]]
 
-else if ((b3 === 'x' || b3 === 'X') && (b5 === 'x' || b5 === 'X') && (b7 === 'x' || b7 === 'X')) {
-  document.getElementById('print').innerHTML = "Player X won";
-    document.getElementById('b1').disabled = true
-    document.getElementById('b2').disabled = true
-    document.getElementById('b4').disabled = true
-    document.getElementById('b6').disabled = true
-    document.getElementById('b8').disabled = true
-    document.getElementById('b9').disabled = true
-    window.alert('player X won!')
-}
-// player 0 wins 
-else if ((b1 === 'o' || b1 === 'O') && (b2 === 'o' || b2 === 'O') && (b3 === 'o' || b3 === 'O')) {
-  document.getElementById('print').innerHTML = "Player O won";
-    document.getElementById('b4').disabled = true
-    document.getElementById('b5').disabled = true
-    document.getElementById('b6').disabled = true
-    document.getElementById('b7').disabled = true
-    document.getElementById('b8').disabled = true
-    document.getElementById('b9').disabled = true
-    window.alert('player O won!')
-}
+    if (cellA == '' || cellB == '' || cellC == '') {
+        continue
+    }
+    if (cellA == cellB && cellB == cellC) {
+      roundWon = true
+      break
+    }
+  }
 
-else if ((b1 === 'o' || b1 === 'O') && (b4 === 'o' || b4 === 'O') && (b7 === 'o' || b7 === 'O')) {
-  document.getElementById('print').innerHTML = "Player O won";
-    document.getElementById('b2').disabled = true
-    document.getElementById('b3').disabled = true
-    document.getElementById('b5').disabled = true
-    document.getElementById('b6').disabled = true
-    document.getElementById('b8').disabled = true
-    document.getElementById('b9').disabled = true
-    window.alert('player O won!')
-}
-
-else if ((b4 === 'o' || b4 === 'O') && (b5 === 'o' || b5 === 'O') && (b6 === 'o' || b6 === 'O')) {
-  document.getElementById('print').innerHTML = "Player O won";
-    document.getElementById('b1').disabled = true
-    document.getElementById('b2').disabled = true
-    document.getElementById('b3').disabled = true
-    document.getElementById('b7').disabled = true
-    document.getElementById('b8').disabled = true
-    document.getElementById('b9').disabled = true
-    window.alert('player O won!')
-}
-
-else if ((b7 === 'o' || b7 === 'O') && (b8 === 'o' || b8 === 'O') && (b9 === 'o' || 93 === 'O')) {
-  document.getElementById('print').innerHTML = "Player O won";
-    document.getElementById('b1').disabled = true
-    document.getElementById('b2').disabled = true
-    document.getElementById('b3').disabled = true
-    document.getElementById('b4').disabled = true
-    document.getElementById('b5').disabled = true
-    document.getElementById('b6').disabled = true
-    window.alert('player O won!')
-}
-
-else if ((b2 === 'o' || b2 === 'O') && (b5 === 'o' || b5 === 'O') && (b8 === 'o' || b8 === 'O')) {
-  document.getElementById('print').innerHTML = "Player O won";
-    document.getElementById('b1').disabled = true
-    document.getElementById('b3').disabled = true
-    document.getElementById('b4').disabled = true
-    document.getElementById('b6').disabled = true
-    document.getElementById('b7').disabled = true
-    document.getElementById('b9').disabled = true
-    window.alert('player O won!')
-}
-
-else if ((b3 === 'o' || b3 === 'O') && (b6 === 'o' || b6 === 'O') && (b9 === 'o' || b9 === 'O')) {
-  document.getElementById('print').innerHTML = "Player O won";
-    document.getElementById('b1').disabled = true
-    document.getElementById('b2').disabled = true
-    document.getElementById('b4').disabled = true
-    document.getElementById('b5').disabled = true
-    document.getElementById('b7').disabled = true
-    document.getElementById('b8').disabled = true
-    window.alert('player O won!')
-}
-
-else if ((b1 === 'o' || b1 === 'O') && (b5 === 'o' || b5 === 'O') && (b9 === 'o' || b9 === 'O')) {
-  document.getElementById('print').innerHTML = "Player O won";
-    document.getElementById('b2').disabled = true
-    document.getElementById('b3').disabled = true
-    document.getElementById('b4').disabled = true
-    document.getElementById('b6').disabled = true
-    document.getElementById('b7').disabled = true
-    document.getElementById('b8').disabled = true
-    window.alert('player O won!')
-}
-
-else if ((b3 === 'o' || b3 === 'O') && (b5 === 'o' || b5 === 'O') && (b7 === 'o' || b7 === 'O')) {
-  document.getElementById('print').innerHTML = "Player O won";
-    document.getElementById('b1').disabled = true
-    document.getElementById('b2').disabled = true
-    document.getElementById('b4').disabled = true
-    document.getElementById('b6').disabled = true
-    document.getElementById('b8').disabled = true
-    document.getElementById('b9').disabled = true
-    window.alert('player O won!')
-}
-// ties
-else if ((b1 == 'X' || b1 == 'O') && (b2 == 'X' || b2 == 'O') && (b3 == 'X' || b3 == 'O') && (b4 == 'X' || b4 == 'O') && (b5 == 'X' || b5 == 'O') && (b6 == 'X' || b6 == 'O') && (b7 == 'X' || b7 == 'O') && (b8 == 'X' || b8 == 'O') && (b9 == 'X' || b9 == 'O')) {
-  document.getElementById('print').innerHTML = "Match Tie"
-  window.alert('Match Tie')
-}
-
-else {
-  if (flag == 1) {
-    document.getElementById('print').innerHTML = "Player X Turn"
+  if (roundWon) {
+    statusText.textContent = `${currentPlayer} wins!`
+    running = false
+  }
+  else if (!options.includes('')) {
+    statusText.textContent = `Draw!`
+    running = false
   }
   else {
-    document.getElementById('print').innerHTML = "Player O Turn"
+    changePlayer()
   }
+
 }
 
-// function to reset game
-function myFunc_2() {
-  location.reload()
-  document.getElementById('b1').value = ''
-  document.getElementById('b2').value = ''
-  document.getElementById('b3').value = ''
-  document.getElementById('b4').value = ''
-  document.getElementById('b5').value = ''
-  document.getElementById('b6').value = ''
-  document.getElementById('b7').value = ''
-  document.getElementById('b8').value = ''
-  document.getElementById('b9').value = ''
+function restartGame() {
+  currentPlayer = 'X'
+  options = [ '', '', '', '', '', '', '', '', '']
+  statusText.textContent = `${currentPlayer}'s turn`
+  cells.forEach(cell => cell.textContent = '')
+  running = true
 }
 
-// functions check turn of player and put x or o in accordingly
-
-
-function myFunc_3(myFunc) {
-  if (flag == 1) {
-    document.getElementById('b1').value = "X"
-    document.getElementById('b1').disabled = true
-    flag = 0
-  }
-else {
-  document.getElementById('b1').value = "O"
-    document.getElementById('b1').disabled = true
-    flag = 1
-  }
-}
-
-function myFunc_4() {
-  if (flag === 1) {
-    document.getElementById('b2').value = "X"
-    document.getElementById('b2').disabled = true
-    flag = 0
-  }
-else {
-  document.getElementById('b2').value = "O"
-    document.getElementById('b2').disabled = true
-    flag = 1
-  }
-}
-
-function myFunc_5() {
-  if (flag === 1) {
-    document.getElementById('b3').value = "X"
-    document.getElementById('b3').disabled = true
-    flag = 0
-  }
-else {
-  document.getElementById('b3').value = "O"
-    document.getElementById('b3').disabled = true
-    flag = 1
-  }
-}
-
-function myFunc_6() {
-  if (flag === 1) {
-    document.getElementById('b4').value = "X"
-    document.getElementById('b4').disabled = true
-    flag = 0
-  }
-else {
-  document.getElementById('b4').value = "O"
-    document.getElementById('b4').disabled = true
-    flag = 1
-  }
-}
-
-function myFunc_7() {
-  if (flag === 1) {
-    document.getElementById('b5').value = "X"
-    document.getElementById('b5').disabled = true
-    flag = 0
-  }
-else {
-  document.getElementById('b5').value = "O"
-    document.getElementById('b5').disabled = true
-    flag = 1
-  }
-}
-
-function myFunc_8() {
-  if (flag === 1) {
-    document.getElementById('b6').value = "X"
-    document.getElementById('b6').disabled = true
-    flag = 0
-  }
-else {
-  document.getElementById('b6').value = "O"
-    document.getElementById('b6').disabled = true
-    flag = 1
-  }
-}
-
-function myFunc_9() {
-  if (flag === 1) {
-    document.getElementById('b7').value = "X"
-    document.getElementById('b7').disabled = true
-    flag = 0
-  }
-else {
-  document.getElementById('b7').value = "O"
-    document.getElementById('b7').disabled = true
-    flag = 1
-  }
-}
-
-function myFunc_10() {
-  if (flag === 1) {
-    document.getElementById('b8').value = "X"
-    document.getElementById('b8').disabled = true
-    flag = 0
-  }
-else {
-  document.getElementById('b8').value = "O"
-    document.getElementById('b8').disabled = true
-    flag = 1
-  }
-}
-
-function myFunc_11() {
-  if (flag === 1) {
-    document.getElementById('b9').value = "X"
-    document.getElementById('b9').disabled = true
-    flag = 0
-  }
-else {
-  document.getElementById('b9').value = "O"
-    document.getElementById('b9').disabled = true
-    flag = 1
-  }
-}
